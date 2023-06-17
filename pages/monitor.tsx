b3d8d0e1dc3a4a22bea195006926ea7ff9cb57ff
dap23 <daffa230601@gmail.com>
2023-06-17 21:58:23 +0700

    fix

--- pages/monitor.tsx
+++ pages/monitor.tsx
@@ -1,11 +1,6 @@
 import Image from "next/image";
-<<<<<<< HEAD
 import { TbTrashFilled } from "react-icons/tb";
-
-export default function Monitor() {
-=======
 import { useEffect, useState } from "react";
-import { TbTrashFilled } from "react-icons/tb";
 
 export default function Monitor() {
   const [organic, setOrganic] = useState<number>(0);
@@ -29,7 +24,6 @@ export default function Monitor() {
     fetchData();
   }, [organic, anorganic]);
 
->>>>>>> 39e7e5c (fix)
   return (
     <div className="relative w-screen md:h-[92vh] h-screen bg-yellow-100 px-10 pt-20 md:px-32 gap-10 flex flex-col md:flex-row items-center justify-center">
       <div className="absolute w-full h-full left-0 top-0 bg-black/50"></div>
@@ -42,9 +36,6 @@ export default function Monitor() {
           className="w-full h-full object-cover"
         />
         <div className="absolute z-20 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
-<<<<<<< HEAD
-          <TbTrashFilled size={40} className="text-red-500" />
-=======
           <TbTrashFilled
             size={40}
             className={`${
@@ -55,7 +46,6 @@ export default function Monitor() {
                 : "text-red-500"
             }`}
           />
->>>>>>> 39e7e5c (fix)
         </div>
       </div>
       <div className="bg-white p-3 w-full md:w-72 shadow-md overflow-hidden h-fit rounded-md z-10">
@@ -75,39 +65,21 @@ export default function Monitor() {
         </div>
         <div className="mt-2 flex items-center justify-center">
           <div className="border-4 border-black rounded-full w-32 h-32 flex items-center justify-center">
-<<<<<<< HEAD
-            <h3 className="text-2xl font-semibold">72%</h3>
-=======
-            <h3 className="text-2xl font-semibold">{capacity ?? 0}%</h3>
->>>>>>> 39e7e5c (fix)
+            <h3 className="text-2xl font-semibold">
+              {organic || anorganic ? capacity : 0}%
+            </h3>
           </div>
         </div>
         <div className="mt-4">
           <span>Trash Category: </span>
           <div className="flex flex-col gap-2 my-4">
             <div className="flex items-center justify-between">
-              <h3>Organic</h3>
-<<<<<<< HEAD
-              <div className="mx-2 text-sm flex items-center justify-center text-white overflow-hidden rounded-md">
-                <button className="shadow-xl shadow-green-500 bg-green-500 w-full p-1">
-                  ON
-                </button>
-                <button className="w-full bg-red-500 p-1">OFF</button>
-              </div>
+              <h3>Anorganic</h3>
+              <span>{organic ?? 0}%</span>
             </div>
             <div className="flex items-center justify-between">
               <h3>Anorganic</h3>
-              <div className="mx-2 text-sm flex items-center justify-center text-white overflow-hidden rounded-md">
-                <button className="bg-green-500 w-full p-1">ON</button>
-                <button className="w-full bg-red-500 p-1">OFF</button>
-              </div>
-=======
-              <span>{organic}%</span>
-            </div>
-            <div className="flex items-center justify-between">
-              <h3>Anorganic</h3>
-              <span>{anorganic}%</span>
->>>>>>> 39e7e5c (fix)
+              <span>{anorganic ?? 0}%</span>
             </div>
           </div>
         </div>
